86260d6753380ee3613bdd95448c7c4d4994a683
kajal yaduvanshi <kajalyaduvanshi529@gmail.com>
2026-09-02 00:15:22 +0530

    Time: 1 ms (95%), Space: 44.2 MB (50.23%) - LeetHub

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+
+        int sr  = source[0];
+        int tr = target[0];
+        int sc = source[1];
+        int tc = target[1];
+
+        if(sr==tr&& sc==tc){
+            return 0;
+        }
+        if(Math.abs(sr-tr)==Math.abs(sc-tc)){
+            return 1;
+        }
+
+        if((sr+sc)%2==(tr+tc)%2){
+            return 2;
+        }
+        return -1;
+        
+    }
+}
