e7e5421f6930dae6f4bb1e36c2e535a6d07eac71
kajal yaduvanshi <kajalyaduvanshi529@gmail.com>
2026-09-22 00:41:46 +0530

    Time: 0 ms (100%), Space: 45.9 MB (87.9%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+
+        double kel = celsius+273.15;
+
+        double fah = celsius*1.80+32.00;
+
+
+        return new double[] {kel,fah};
+        
+    }
+}
